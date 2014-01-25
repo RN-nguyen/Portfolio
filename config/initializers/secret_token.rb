65fc725496169fe8066fccf042db36fa0564ced5
Rebecca Nguyen <rebeccanguyen@128-110-237-238.uguest.utah.edu>
2014-01-25 16:56:54 -0700

    initial start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = 'c4676d5f3403342589c163587d2cf0b3479059d314406f749f5aa6c391275525de46a2907acc3ab217729d6d39c7f290ee9a5a91089acf0645c17a4ad11c72ee'
